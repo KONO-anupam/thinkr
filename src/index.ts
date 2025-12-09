ed10e2c1a6ef0c2ce3115ff42d0677508afd86e1
Kono <anupam.nitrkl@gmail.com>
2025-12-10 02:41:16 +0530

    feat: created database schema

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,81 @@
 import express from "express";
 import mongoose  from "mongoose";
 import jwt from "jsonwebtoken"
+import { userModel } from "./db.js";
+import { useMatch } from "react-router-dom";
 
 const app = express();
+app.use(express.json());
 
-app.post("api/v1/signup", (req,res)=>{
+const JWT_PASSWORD = "123abc"
+
+app.post("/api/v1/signup", async (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{
+    await userModel.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "user signed up!"
+    })
+    }
+    catch(e){
+        res.status(500).json({
+            message: "user already exists"
+        })
+    }
+})
+
+app.post("/api/v1/signin",async (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await userModel.findOne({
+        username,
+        password
+    })
+    if(existingUser){
+        const token = jwt.sign({
+            id: existingUser._id
+        },JWT_PASSWORD)
+
+        res.json({
+           message: 'you are signed in' 
+        })
+    }
+    else{
+        res.status(403).json({
+            message: "incorrect credentials"
+        })
+    }
+
 
 })
 
-app.post("api/v1/signin", (req,res)=>{
+app.post("/api/v1/content", (req,res)=>{
 
 })
 
-app.post("api/v1/content", (req,res)=>{
+app.get("/api/v1/content", (req,res)=>{
 
 })
 
-app.get("api/v1/content", (req,res)=>{
+app.delete("/api/v1/content", (req,res)=>{
 
 })
 
-app.delete("api/v1/content", (req,res)=>{
+app.post("/api/v1/brain/share", (req, res)=>{
+
+})
+
+app.get("/api/v1/brain/:shareLink", (req,res)=>{
 
 })
 
 
 
-
+app.listen(3000);
